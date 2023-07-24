e407548888568f17a82c5ecc5d2512ece078287f
Juan Ezequiel Antonio <ezequiel.antonio1987@gmail.com>
2023-07-23 21:20:21 -0300

    uno

--- TPFinal/src/componentes/Home/Home.jsx
+++ TPFinal/src/componentes/Home/Home.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div><h2>Home de todos los envases</h2>
-    <p>Aca puedo trenderizar todo mi inventario</p></div>
+  return (<div></div>
+    
   )
 }
 
